2a0fa2b3ea19908031eb540cbbe1ebfebf60cb4a
Roger <luojie.5408@163.com>
2018-06-27 16:30:37 +0800

    update

--- webpack.config/sizeTest/testApp.js
+++ webpack.config/sizeTest/testApp.js
@@ -7,7 +7,10 @@ import React from 'react' // 8 kb
 // import { Editor } from 'draft-js' //193kb
 //  import { createStore, applyMiddleware } from 'redux' // 8kb
 // import "regenerator-runtime/runtime"; // 7kb
-
+// import 'isomorphic-fetch' // 7.5kb
+// import { polyfill } from 'es6-promise' // 8.5kb
+// polyfill()
+// import 'whatwg-fetch' // 7kb
 class App extends React.Component {
     constructor(props) {
         super(props)
